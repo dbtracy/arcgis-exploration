facda04d360361e93054ba8807452f07f4761f64
Dan Tracy <daniel.benjamin.tracy@gmail.com>
2019-09-26 16:02:59 -0400

    FEAT(html): wired styling to html, map rendered

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
-console.log('hi there')
+require(["esri/Map", "esri/views/MapView"], function (Map, MapView) {
+  let map = new Map({
+    basemap: 'streets'
+  })
+
+  let view = new MapView({
+    container: 'viewDiv',
+    map: map
+  })
+});
